585b66fd2ffd68afccc69d538ba755510d476507
saurabh47 <saurabhgangamwar78@gmail.com>
2018-01-21 23:46:03 +0530

    Using Put Method instead of Get

--- app/src/main/java/com/webdestro/www/examseating/MainActivity.java
+++ app/src/main/java/com/webdestro/www/examseating/MainActivity.java
@@ -1,11 +1,9 @@
 package com.webdestro.www.examseating;
 
-import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,14 +15,15 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.unstoppable.submitbuttonview.SubmitButton;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.json.JSONArray;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
     //String url = "http://blynk-cloud.com/f391b4bce2804777b46cc3fa1152e818/update/v4";
-    String Base_url="http://blynk-cloud.com/f391b4bce2804777b46cc3fa1152e818/update/v2?value=";
-    String url,value;
+    String Base_url="http://blynk-cloud.com/90b1a2dad1ce4c5b9aad0d0557c1c375/update/v1";
+    String url,value,row1,row2;
     //String url="http://blynk-cloud.com/f391b4bce2804777b46cc3fa1152e818/get/v4";
     //Button submitButton;
     SubmitButton submitButton;
@@ -51,9 +50,11 @@ public class MainActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                value=a.getText().toString()+"%20"+b.getText().toString()+"%20"+c.getText().toString()+"%20"+d.getText().toString()+
-                        "%20"+e.getText().toString()+"%20"+f.getText().toString()+"%20"+g.getText().toString()+"%20"+h.getText().toString();
-                url=Base_url+value;
+               // value=a.getText().toString()+"%20"+b.getText().toString()+"%20"+c.getText().toString()+"%20"+d.getText().toString()+
+                //        "%20"+e.getText().toString()+"%20"+f.getText().toString()+"%20"+g.getText().toString()+"%20"+h.getText().toString();
+                row1=a.getText().toString()+" "+b.getText().toString()+" "+c.getText().toString()+" "+d.getText().toString();
+                row2=e.getText().toString()+" "+f.getText().toString()+" "+g.getText().toString()+" "+h.getText().toString();
+                url=Base_url;//+value;
                 Log.d("URL",url);
                 volleyRq();
             }
@@ -75,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
     public void volleyRq(){
 //        final ProgressDialog loading = ProgressDialog.show(this, "","Checking Details",false,false);
 
-        StringRequest putRequest = new StringRequest(Request.Method.GET, url,
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
                 new Response.Listener<String>()
                 {
                     @Override
@@ -102,16 +103,29 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         ) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
 
-//            @Override
-//            protected Map<String, String> getParams()
-//            {
+            @Override
+            public byte[] getBody()
+            {
 //                Map<String, String>  params = new HashMap<String, String>();
 //                params.put("name", "Alif");
 //                params.put("domain", "http://itsalif.info");
-//
-//                return params;
-//            }
+                JSONArray mJSONArray=new JSONArray();
+
+                mJSONArray.put(row1);
+                    mJSONArray.put(row2);
+                try {
+                    Log.i("json", mJSONArray.toString());
+                    return mJSONArray.toString().getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
 
         };
         RequestQueue queue = Volley.newRequestQueue(this); // this = context
